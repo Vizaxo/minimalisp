9f113e643210f0790ae6c777de030bb03dc9dc10
Vizaxo <vizaxo@mailfence.com>
2018-01-10 19:07:58 +0000

    Add lambda special form

--- Parser.hs
+++ Parser.hs
@@ -5,24 +5,17 @@ import Text.Parsec.Char
 import Text.Parsec hiding (Error)
 import Types
 
-parseExpr input = result $ parse expr "" input
+parseExpr input = result $ parse multipleExpr "" input
   where result (Left err) = Error $ show err
         result (Right e)  = e
 
-expr :: Parser Expr
---expr = (Multiple <$> (list <$> expr <*> ((many space) *> expr))) <|> (sexp <|> integerLit <|> symbol <* (many space))
-  --where list a b = [a, b]
+multipleExpr = Multiple <$> many1 expr
 
---expr = (multiple <$> (singleExpr <*> ((many space) *> expr))) <|> singleExpr
---expr = (multiple <$> (singleExpr <*> singleExpr)) <|> singleExpr
-expr = try (multiple <$> singleExpr <* (many space) <*> expr) <|> (many space) *> singleExpr <* (many space)
-  where multiple a (Multiple bs) = Multiple (a:bs)
-        multiple a b             = Multiple [a, b]
+expr :: Parser Expr
+expr = (many space) *> singleExpr <* (many space)
 
 sexp :: Parser Expr
-sexp = cons <$> (between (char '(') (char ')')) expr
-  where cons (Multiple es) = Cons es
-        cons e             = Cons [e]
+sexp = between (char '(') (char ')') $ Cons <$> (many1 expr) <|> Nil <$ (many space)
 
 singleExpr :: Parser Expr
 singleExpr = (many space) *> (try nil <|> sexp <|> integerLit  <|> symbol) <* (many space)
